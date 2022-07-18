4a1d7f14def817ea3ded768466c0774f8910c0e7
llr2ll <raphaelsanseverino@gmail.com>
2022-07-18 14:21:46 -0300

    'correção'

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,7 +9,29 @@
   				<label for="filtrar-tabela">Filtre:</label>
   				<input type="text" name="filtro" id="filtrar-tabela" placeholder="Digite o nome">
   				
-				<User />
+				<Table>
+					 		<thead>
+  				<tr>
+  					<th>Nome</th>
+  					<th>Data de Nacimento</th>
+  				</tr>
+  			</thead>
+            
+  			    <tbody id="tabela-pacientes">
+                    
+  				<tr v-for='usuario in usuarios' :key='usuario.id' class="paciente" id="primeiro-paciente">
+  						<td class="info-nome">{{usuario.name}}</td>
+  						<td class="info-nascimento">
+						    <p>{{usuario.date}}</p>
+							<div>
+								<router-link :to="`/edit/${usuario.name}`"><button class='btns'><img class='img' src='/pencil.svg' /></button></router-link>
+								<button class='btns'><img class='img' src='/trash.png' /></button>
+							</div>
+                		</td>
+  				</tr>
+
+  			</tbody>
+				</Table>
           
   				<span id="erro-ajax" class="invisivel">Erro ao buscar os pacientes</span>
   
@@ -24,22 +46,41 @@
   
  import Header from '../components/Header.vue'
  import Button from '../components/Button.vue'
- import User from '../components/User.vue'
 
  export default {
     components: {
     Header,
     Button,
-    User
 	},
+	data() {
+            return { usuarios: [
+                    {id:1 , name:'Paulo',date:'08/12/1990'},
+                    {id:2 , name:'Erika',date:'10/03/1977'},
+                    {id:3 , name:'Roger',date:'22/08/1984'}
+                ]
+            }
+        }
 }
   
 </script>
 
 <style scoped>
   
+  
   .container{ width: 60%; height: 100%; margin: 5% auto; }
   
+  table{ width: 100%; margin-bottom: .5em; table-layout: fixed; }
+
+  td, th { padding: .7em; margin: 0; border: 1px solid #ccc; text-align: center; }
+  
+  th{ font-weight: bold; background-color: #EEE; }
+
+  .info-nascimento{ display: flex; align-items: center; justify-content: center; justify-content: space-between;}
+  
+  .img{ width: 20px; height: 20px;}
+  
+  .btns{margin: 0 5px; }
+
   section{ margin: 2em 0; overflow: hidden; }
   
   section h2{ font-size: 3em; display: block; padding-bottom: .5em; border-bottom: 1px solid #ccc; margin-bottom: .5em; }
