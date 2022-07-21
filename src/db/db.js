030b7f20bf5bafbdbacc0ae41db69ade1e2fd655
GDM Raphael <GDM Raphael@MKT05.ad.unimes.br>
2022-07-21 15:24:09 -0300

    db-fix

--- src/db/db.js
+++ src/db/db.js
@@ -1,12 +1,12 @@
 const mysql = require("mysql2/promise")
 
 const pool = mysql.createPool({
-	host: '',
-	user: '',
-	password: '',
-	port: 3006,
+	host: '127.0.0.1',
+	user: 'root',
+	password: 'p@ssw0rd',
+	port: 3306,
 	database: 'dg_solutions',
-	ssl: {} 
+	//ssl: {} 
 })
 
 module.exports = pool
