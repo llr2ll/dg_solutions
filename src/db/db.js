78b83b5ca1b36974b1f7bcf3df3d5c58d76304db
GDM Raphael <GDM Raphael@MKT05.ad.unimes.br>
2022-07-21 14:25:51 -0300

    version_1.0

--- src/db/db.js
+++ src/db/db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2/promise")
 
 const pool = mysql.createPool({
-	host: 't3071viq0k1g.aws-sa-east-1-1.psdb.cloud',
-	user: 'vwaxy92qd21d',
-	password: 'pscale_pw_4qmbc9WaSULcewRqInmX3jePRHATaAvEqy9F7pAtvxg',
-	port: 3306,
+	host: '',
+	user: '',
+	password: '',
+	port: 3006,
 	database: 'dg_solutions',
 	ssl: {} 
 })
